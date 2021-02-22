5a4409b85662fe67e80c6a99ddd1eb93f4faf3e4
aleksandr <alexbratchin@gmail.com>
2021-02-22 15:20:56 +0300

    second

--- js/pole.js
+++ js/pole.js
@@ -3,9 +3,15 @@ let ROW = 12;
 
 let arr;
 
+$(window).resize(function () {
+  resizeCols();
+});
+
 function start() {
   arr = new Arr(COL, ROW);
   console.log(arr);
+  drawPole(arr.poles);
+  drawAlgorithm(arr.algorithm);
 }
 
 class Arr {
@@ -52,7 +58,7 @@ class Arr {
       }
 
       switch (v) {
-        case 'left':
+        case 'right':
           {
             j++;
           }
@@ -67,7 +73,7 @@ class Arr {
             i++;
           }
           break;
-        case 'right':
+        case 'left':
           {
             j--;
           }
@@ -96,7 +102,7 @@ class Arr {
 
   #vector(arr, i, j) {
     if (j + 1 < arr[i].length && arr[i][j] + 1 == arr[i][j + 1]) {
-      return 'left';
+      return 'right';
     }
     if (i - 1 >= 0 && arr[i][j] + 1 == arr[i - 1][j]) {
       return 'top';
@@ -105,23 +111,37 @@ class Arr {
       return 'bottom';
     }
     if (j - 1 >= 0 && arr[i][j] + 1 == arr[i][j - 1]) {
-      return 'right';
+      return 'left';
     }
     return '';
+  }
+
+  #getRandomArbitrary(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
   }
 
   #pole(shab, i, j) {
     let arr = {};
     let col = shab.length;
     let row = shab[0].length;
-    /* arr.i = i;
-    arr.j = j; */
     arr.start = false;
     arr.finish = false;
 
     if (shab[i][j] == 0) {
       //Это преграда
       arr.type = 'преграда';
+      switch (this.#getRandomArbitrary(1, 3)) {
+        case 1:
+          arr.subtype = 'преграда1';
+          break;
+        case 2:
+          arr.subtype = 'преграда2';
+          break;
+        case 3:
+          arr.subtype = 'преграда3';
+          break;
+      }
+
       shab.length;
     } else {
       // это тропа
@@ -129,9 +149,13 @@ class Arr {
       arr.vector = this.#vector(shab, i, j);
       if (shab[i][j] == 1) {
         //это старт
+        arr.type = 'старт';
+        arr.subtype = 'старт';
         arr.start = true;
       } else if (shab[i][j] == this.#maxValue(shab)) {
         //это финиш
+        arr.type = 'финиш';
+        arr.subtype = 'финиш';
         arr.finish = true;
       } else if (
         j != 0 &&
@@ -179,6 +203,96 @@ class Arr {
     }
     return arr;
   }
+}
+
+function drawPole(arPoles) {
+  let colRows = arPoles.length;
+  let colCols = arPoles[0].length;
+  let str = '';
+  for (let i = 0; i < colRows; i++) {
+    str += "<div class='row'>";
+    for (let j = 0; j < colCols; j++) {
+      str += `<div class="col-xs-${12 / colCols}">`;
+      switch (arPoles[i][j].subtype) {
+        case 'преграда1':
+          {
+            str += `<div class="pole stop1"></div>`;
+          }
+          break;
+        case 'преграда2':
+          {
+            str += `<div class="pole stop2"></div>`;
+          }
+          break;
+        case 'преграда3':
+          {
+            str += `<div class="pole stop3"></div>`;
+          }
+          break;
+        case 'старт':
+          {
+            str += `<div class="pole start"></div>`;
+          }
+          break;
+        case 'финиш':
+          {
+            str += `<div class="pole finish"></div>`;
+          }
+          break;
+      }
+      str += `</div>`;
+    }
+    str += '</div>';
+  }
+  $('#pole').html(str);
+  resizeCols();
+}
+
+function drawAlgorithm(arAlgorithm) {
+  let str = '';
+  for (let i = 0; i < arAlgorithm.length; i++) {
+    console.info(arAlgorithm[i]);
+
+    switch (arAlgorithm[i]['vector']) {
+      case 'left':
+        {
+          str += `<div class="col1 col-xs-1 algorithmItem">
+                <i class="fa fa-arrow-left"></i> ${arAlgorithm[i].kol}
+                </div>`;
+        }
+        break;
+      case 'right':
+        {
+          str += `<div class="col1 col-xs-1 algorithmItem">
+                <i class="fa fa-arrow-right"></i> ${arAlgorithm[i].kol}
+                </div>`;
+        }
+        break;
+      case 'top':
+        {
+          str += `<div class="col1 col-xs-1 algorithmItem">
+                <i class="fa fa-arrow-up"></i> ${arAlgorithm[i].kol}
+                </div>`;
+        }
+        break;
+      case 'bottom':
+        {
+          str += `<div class="col1 col-xs-1 algorithmItem">
+                <i class="fa fa-arrow-down"></i> ${arAlgorithm[i].kol}
+                </div>`;
+        }
+        break;
+    }
+  }
+  $('#algorithm .row').html(str);
+  console.info($('#algorithm .row').width());
+  console.info($('#algorithm .row>div').length);
+  console.info(arAlgorithm.length);
+}
+
+function resizeCols() {
+  let colsWidth = $('#pole').find('.row:first-child div:first-child').width();
+  $('#pole').find('.row>div').height(colsWidth);
 }
 
 start();
